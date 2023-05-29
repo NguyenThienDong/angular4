bb7073009c806634bc8d3953a861fbaa80e46375
dongnguyen <nguyenthiendong17031998@gmail.com>
2023-05-30 02:42:34 +0700

    bai 8: tao component bang command line cli: ng g c tencomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
+import { BookComponent } from './book/book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
+    BookComponent,
   ],
   imports: [
     BrowserModule,
