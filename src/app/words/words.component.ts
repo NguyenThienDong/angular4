528af8f8b12ac94dc3e3e86388ad6ecb9d078fa1
dongnguyen <nguyenthiendong17031998@gmail.com>
2023-05-31 01:37:56 +0700

    bai 19: them phan tu vao mang

--- src/app/words/words.component.ts
+++ src/app/words/words.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./words.component.css']
 })
 export class WordsComponent {
+  newEn = '';
+  newVn = '';
   arrWords = [
     {id: 1, en: 'action', vi: 'hành động', memorized: true},
     {id: 2, en: 'actor', vi: 'diễn viên', memorized: false},
@@ -14,4 +16,16 @@ export class WordsComponent {
     {id: 5, en: 'bath', vi: 'tắm', memorized: false},
     {id: 6, en: 'bedroom', vi: 'phòng ngủ', memorized: true},
   ]
+
+  // Them tu moi
+  addWord() {
+    this.arrWords.unshift({
+      id: this.arrWords.length + 1,
+      en: this.newEn,
+      vi: this.newVn,
+      memorized: false,
+    });
+    this.newEn = '';
+    this.newVn = '';
+  }
 }
