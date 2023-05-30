4a7866b29d2b151753ea5261641eef2f2ada14ec
Dong <dongnt1@toprate.io>
2023-05-30 11:34:41 +0700

    bai 11: xu ly text input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WordComponent } from './word/word.component';
 import { BookComponent } from './book/book.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WordComponent,
     BookComponent,
+    UserFormComponent,
   ],
   imports: [
     BrowserModule,
